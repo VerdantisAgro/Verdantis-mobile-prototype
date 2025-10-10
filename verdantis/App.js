d67dc7775430d02bb3acf2a952bad0c5f409f4fd
caiolucasxz55 <caiolucasxz55@gmail.com>
2025-10-10 20:40:19 -0300

    Feat: app.js

--- verdantis/App.js
+++ verdantis/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from "react";
+import { StatusBar, View, ActivityIndicator } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Ionicons";
+
+import AppRoutes from "./routes/appRoutes";
+import AuthProvider, { AuthContext } from "./context/AuthContext";
+Icon.loadFont();
+
+function AppContent() {
+  const { loading } = React.useContext(AuthContext);
+
+  
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#2e7d32" />
+      </View>
+    );
+  }
+
+  return <AppRoutes />;
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      
+      <AuthProvider>
+        <StatusBar barStyle="light-content" backgroundColor="#0a0a0a" />
+        <AppContent />
+      </AuthProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
